be6f7cf1b6b5a863000de49499fd4a320f5dfb37
Tyler Grow <tyler@pbbbl.com>
2022-03-30 14:23:30 -0600

    working on github deploy

--- functions/api.secrets.js
+++ functions/api.secrets.js
@@ -1,6 +1,6 @@
 module.exports = (api) => {
   console.log("api", api.post);
-  return api.post("/secrets/dplrwh", async (req, res) => {
+  return api.post("/doppler/dplrwh", async (req, res) => {
     // const accessToken =
     //   "440e7b09edf8ef7ab35b90134974d58426be2954e24272174bd5cf72858630caf1ef2aaae897ccb52bfa363700b9b352fb64";
     // let body = await req.body;
